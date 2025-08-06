f61f1750f24cb0eeb992410bdb849532a4d07d02
arduriki <jordi.ardura.professional@gmail.com>
2025-08-06 15:23:43 +0200

    fps limited to 60

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
